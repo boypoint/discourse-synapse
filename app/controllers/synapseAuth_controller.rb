e8c4469e77c6537dcdf645ab9c9685de7ecc2dab
boypoint <github@boypoint.de>
2019-11-10 20:49:11 +0100

    Update synapseAuth_controller.rb

--- app/controllers/synapseAuth_controller.rb
+++ app/controllers/synapseAuth_controller.rb
@@ -1,5 +1,6 @@
 class SynapseauthController < ApplicationController
   skip_before_action :verify_authenticity_token
+  skip_before_filter :check_xhr
 
   def index
     return_msg = {"auth": {"success": false}};
