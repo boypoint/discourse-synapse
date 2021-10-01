28bbaab407a3cfe13f413236b66a3ce877f81724
nobodyfrm <nobodyfrm@gmail.com>
2021-10-01 21:38:41 +0200

    downcase mxid in response

--- app/controllers/synapseAuth_controller.rb
+++ app/controllers/synapseAuth_controller.rb
@@ -6,9 +6,9 @@ class SynapseauthController < ApplicationController
     inputData = JSON.parse request.body.read;
 
     if(inputData['user'].present? && inputData['user']['id'].present? && inputData['user']['password'].present?)
-      authUser = User.find_by_username(inputData['user']['id'].split(":")[0][1..-1].downcase);
+      authUser = User.find_by_username(inputData['user']['id'].split(":")[0][1..-1]);
       if(authUser.present? && authUser.confirm_password?(inputData['user']['password']) && authUser.trust_level >= 1)
-        return_msg = {"auth": {"success": true, "mxid": inputData['user']['id'], "profile": {"display_name": authUser.username}}};
+        return_msg = {"auth": {"success": true, "mxid": inputData['user']['id'].downcase, "profile": {"display_name": authUser.username}}};
       end
     end
 
